c0cb651e3a42274cbcd1568ddb38523fa0610a0a
Marlon Gomes Lopes <marlonglopes@gmail.com>
2010-11-15 21:12:23 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cafofetis::Application.config.secret_token = 'b150c38ef5d744fe3b9e78c51885f103aba3694f4eb790e01b52f902d42fc042383e1caf373d587428ec3e3accf837bf070fd9b4f540e87da52f1255767f509a'
